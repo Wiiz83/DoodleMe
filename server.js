bddf9b92e4633731f8b4f8aecefb30ea9c12e595
Mahdi <mahdi.hentati@gmail.com>
2017-04-25 22:46:47 +0200

    commit

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-var express  = require('express'),
-    path     = require('path'),
+var express = require('express'),
+    path = require('path'),
     bodyParser = require('body-parser'),
     app = express(),
     expressValidator = require('express-validator');
 
 
-/*Set EJS template Engine*/
-app.set('views','./views');
-app.set('view engine','ejs');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true })); //support x-www-form-urlencoded
@@ -15,22 +13,22 @@ app.use(bodyParser.json());
 app.use(expressValidator());
 
 /*MySql connection*/
-var connection  = require('express-myconnection'),
+var connection = require('express-myconnection'),
     mysql = require('mysql');
 
 app.use(
 
-    connection(mysql,{
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'test',
-        debug    : false //set true if you wanna see debug logger
-    },'request')
+    connection(mysql, {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'test',
+        debug: false //set true if you wanna see debug logger
+    }, 'request')
 
 );
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.send('Welcome');
 });
 
@@ -46,74 +44,73 @@ var router = express.Router();
 *  we can use this for doing validation,authetication
 *  for every route started with /api
 --------------------------------------------------------*/
-router.use(function(req, res, next) {
-    console.log(req.method, req.url);
-    next();
-});
+
 
 var curut = router.route('/user');
 
 
 //show the CRUD interface | GET
-curut.get(function(req,res,next){
+curut.get(function (req, res) {
 
 
-    req.getConnection(function(err,conn){
+    req.getConnection(function (err, conn) {
 
-        if (err) return next("Cannot Connect");
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
 
-        var query = conn.query('SELECT * FROM t_user',function(err,rows){
+        var query = conn.query('SELECT * FROM t_user', function (err, rows) {
 
-            if(err){
+            if (err) {
                 console.log(err);
-                return next("Mysql error, check your query");
+                res.sendStatus(500);
             }
-
-            res.render('user',{title:"RESTful Crud Example",data:rows});
-
-         });
+            else
+                res.json(rows);
+        });
 
     });
 
 });
 //post data to DB | POST
-curut.post(function(req,res,next){
+curut.post(function (req, res, next) {
 
     //validation
-    req.assert('name','Name is required').notEmpty();
-    req.assert('email','A valid email is required').isEmail();
-    req.assert('password','Enter a password 6 - 20').len(6,20);
+    req.assert('name', 'Name is required').notEmpty();
+    req.assert('email', 'A valid email is required').isEmail();
+    req.assert('password', 'Enter a password 6 - 20').len(6, 20);
 
     var errors = req.validationErrors();
-    if(errors){
+    if (errors) {
         res.status(422).json(errors);
         return;
     }
 
     //get data
     var data = {
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-     };
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    };
 
     //inserting into mysql
-    req.getConnection(function (err, conn){
+    req.getConnection(function (err, conn) {
 
         if (err) return next("Cannot Connect");
 
-        var query = conn.query("INSERT INTO t_user set ? ",data, function(err, rows){
+        var query = conn.query("INSERT INTO t_user set ? ", data, function (err, rows) {
 
-           if(err){
+            if (err) {
                 console.log(err);
                 return next("Mysql error, check your query");
-           }
+            }
 
-          res.sendStatus(200);
+            res.sendStatus(200);
 
         });
 
-     });
+    });
 
 });
 
@@ -128,33 +125,33 @@ a validation everytime route /api/user/:user_id it hit.
 
 remove curut2.all() if you dont want it
 ------------------------------------------------------*/
-curut2.all(function(req,res,next){
+curut2.all(function (req, res, next) {
     console.log("You need to smth about curut2 Route ? Do it here");
     console.log(req.params);
     next();
 });
 
 //get data to update
-curut2.get(function(req,res,next){
+curut2.get(function (req, res, next) {
 
     var user_id = req.params.user_id;
 
-    req.getConnection(function(err,conn){
+    req.getConnection(function (err, conn) {
 
         if (err) return next("Cannot Connect");
 
-        var query = conn.query("SELECT * FROM t_user WHERE user_id = ? ",[user_id],function(err,rows){
+        var query = conn.query("SELECT * FROM t_user WHERE user_id = ? ", [user_id], function (err, rows) {
 
-            if(err){
+            if (err) {
                 console.log(err);
                 return next("Mysql error, check your query");
             }
 
             //if user not found
-            if(rows.length < 1)
+            if (rows.length < 1)
                 return res.send("User Not found");
 
-            res.render('edit',{title:"Edit user",data:rows});
+            res.render('edit', { title: "Edit user", data: rows });
         });
 
     });
@@ -162,77 +159,77 @@ curut2.get(function(req,res,next){
 });
 
 //update data
-curut2.put(function(req,res,next){
+curut2.put(function (req, res, next) {
     var user_id = req.params.user_id;
 
     //validation
-    req.assert('name','Name is required').notEmpty();
-    req.assert('email','A valid email is required').isEmail();
-    req.assert('password','Enter a password 6 - 20').len(6,20);
+    req.assert('name', 'Name is required').notEmpty();
+    req.assert('email', 'A valid email is required').isEmail();
+    req.assert('password', 'Enter a password 6 - 20').len(6, 20);
 
     var errors = req.validationErrors();
-    if(errors){
+    if (errors) {
         res.status(422).json(errors);
         return;
     }
 
     //get data
     var data = {
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-     };
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    };
 
     //inserting into mysql
-    req.getConnection(function (err, conn){
+    req.getConnection(function (err, conn) {
 
         if (err) return next("Cannot Connect");
 
-        var query = conn.query("UPDATE t_user set ? WHERE user_id = ? ",[data,user_id], function(err, rows){
+        var query = conn.query("UPDATE t_user set ? WHERE user_id = ? ", [data, user_id], function (err, rows) {
 
-           if(err){
+            if (err) {
                 console.log(err);
                 return next("Mysql error, check your query");
-           }
+            }
 
-          res.sendStatus(200);
+            res.sendStatus(200);
 
         });
 
-     });
+    });
 
 });
 
 //delete data
-curut2.delete(function(req,res,next){
+curut2.delete(function (req, res, next) {
 
     var user_id = req.params.user_id;
 
-     req.getConnection(function (err, conn) {
+    req.getConnection(function (err, conn) {
 
         if (err) return next("Cannot Connect");
 
-        var query = conn.query("DELETE FROM t_user  WHERE user_id = ? ",[user_id], function(err, rows){
+        var query = conn.query("DELETE FROM t_user  WHERE user_id = ? ", [user_id], function (err, rows) {
 
-             if(err){
+            if (err) {
                 console.log(err);
                 return next("Mysql error, check your query");
-             }
+            }
 
-             res.sendStatus(200);
+            res.sendStatus(200);
 
         });
         //console.log(query.sql);
 
-     });
+    });
 });
 
 //now we need to apply our router here
 app.use('/api', router);
 
 //start Server
-var server = app.listen(3000,function(){
+var server = app.listen(3000, function () {
 
-   console.log("Listening to port %s",server.address().port);
+    console.log("Listening to port %s", server.address().port);
 
 });
