a840ed5c8db528261eda0ad685ba6c8bdc4ff789
imaneet <dav.toutain31@free.fr>
2017-04-29 16:35:28 +0200

    Controller (bug restants)
    Controller rest avec fichiers 'class' pour les objets et bugs node sur
    le controller

---
+++ CTRL_rest/user.js
@@ -0,0 +1,47 @@
+var listeUsers = {};
+
+function utilisateur(id, nom, prenom, pseudo, mdp){
+	this.nom = nom;
+	this.prenom = prenom;
+	this.pseudo = pseudo;
+	this.mdp = mdp;
+}
+
+var newUser = function(id, nom, prenom, pseudo, mdp){
+	if (typeof listeUsers[id] != 'undefined') {
+		listeUsers[id] = new utilisateur(id, desc, date, pseudo, mdp);
+		return 1;
+    }
+    return 0;
+}
+
+var getNom = function(id){
+	if (typeof listeUsers[id] != 'undefined'){
+		return listeUsers[id].nom;
+	}
+}
+
+var getPrenom = function(id){
+	if (typeof listeUsers[id] != 'undefined'){
+		return listeUsers[id].prenom;
+	}
+}
+
+var uConnect = function(pseudo, mdp){
+	var found = 0;
+	var idUser;
+	for (var u in listeUsers){
+		if(u.pseudo == pseudo && u.mdp == mdp){
+			found = 1;
+			idUser = u.id;
+			break;
+		}
+	}
+	if(found){
+		return listeUsers[idUser];
+	}
+}
+
+var modificationUser = function(pseudo, mdp, nom, prenom, idUser){
+	this.listeUsers[idUser] = new utilisateur(idUser, nom, prenom, pseudo, mdp);
+}
