5de23c8dd1b38d260e721beac85dd1437b1c329f
Mahdi <mahdi.hentati@gmail.com>
2017-05-07 20:53:57 +0200

    erreurs

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -31,33 +31,33 @@ users_route.post(function (req, res) {
     var data = [user.firstName, user.lastName, user.pseudo, user.password];
     for (var i = 0; i < data.length; i++)
         if (data[i] == undefined) {
-            return res.status(400).send({ status: "ERREUR", description: "Requete mal formattée" });
+            return res.status(400).send({ status: "Erreur", description: "Requete mal formattée" });
         }
     data[3] = hash(user.password);
     req.getConnection(function (err, conn) {
         if (err)
-            return res.status(500).send({ status: "ERREUR", description: "Problème de connexion à la base de données"});
+            return res.status(500).send({ status: "Erreur", description: "Problème de connexion à la base de données"});
         // insertion
         var query = conn.query("SELECT * FROM users WHERE pseudo=?",
             user.pseudo, function (err, result) {
                 if (err) {
                     console.log(query.sql);
                     console.log(err);
-                    return res.status(500).send({ status: "ERREUR", description: err });
+                    return res.status(500).send({ status: "Erreur", description: err });
                 }
                 else
                     if (result.length > 0)
-                        return res.status(409).send({ "status": "ERREUR", "description": "L'utilisateur existe déja" });
+                        return res.status(409).send({ "status": "Erreur", "description": "L'utilisateur existe déja" });
                     else {
                         var query = conn.query("INSERT INTO users (firstName, lastName, pseudo, passHash)  VALUES (?,?,?,?)",
                             data, function (err, result) {
                                 if (err) {
                                     console.log(query.sql);
                                     console.log(err);
-                                    return res.status(500).send({ status: "ERREUR", description: err.message });
+                                    return res.status(500).send({ status: "Erreur", description: err.message });
                                 }
                                 else
-                                    return res.redirect("/login.html");
+                                    return res.send({ status: "Succès"}); 
                             });
                     }
             });
