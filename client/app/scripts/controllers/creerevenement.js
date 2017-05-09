a89c3c98f7259ea113e0e9a1a0fb7f5b0505cfe4
Wiiz83 <uzanlah@gmail.com>
2017-05-09 10:53:32 +0200

    Create event

--- client/app/scripts/controllers/creerevenement.js
+++ client/app/scripts/controllers/creerevenement.js
@@ -1,7 +1,22 @@
 'use strict';
 
  angular.module('clientApp')
- .controller('CreerEvenementCtrl', function ($scope, $window, $rootScope, $location, $routeParams) {
+ .controller('CreerEvenementCtrl', function ($scope, $window, $rootScope, $location, $routeParams, eventService) {
+
+   $scope.template = "./views/includes/createEventStep1.html";
+
+ 	$scope.createEvent = function(){
+        var objToSave = new eventService();
+        objToSave.title = $scope.title;
+        objToSave.address = $scope.address;
+        objToSave.description = $scope.description;
+
+        objToSave.$create(function(response) {
+            $scope.template = "./views/includes/createEventStep2.html";
+        }, function(response) {
+          $scope.errorMessage = response.data.description;
+        });
+     };
 
 
 });
