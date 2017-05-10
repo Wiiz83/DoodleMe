a5ee4224ba9abb385ba65e15b198a7e6703bbd58
Mahdi <mahdi.hentati@gmail.com>
2017-05-10 22:57:58 +0200

    -

--- client/app/scripts/services/event.js
+++ client/app/scripts/services/event.js
@@ -3,7 +3,7 @@
 angular.module('clientApp')
 .factory('FactoryEvent', function ($resource) {
 	return $resource('http://localhost:3000/api/events/:id', {}, {
-		getEvent: { method: 'GET', params:{id:'@id'}, isArray: true, cancellable: true}, 
+		getEvent: { method: 'GET', params:{id:'@id'}, cancellable: true}, 
         update: {method: 'PUT'}
 	});
 });
--- client/app/scripts/services/events.js
+++ client/app/scripts/services/events.js
@@ -5,7 +5,7 @@ angular.module('clientApp')
 	return $resource('http://localhost:3000/api/events', {}, {
 		create: { method: 'POST' },
         update: { method: 'PUT', params:{id:'@id'} },
-		get: { method: 'GET', params:{id:'@id'}, isArray: true, cancellable: true },
+		get: { method: 'GET', params:{id:'@id'}, cancellable: true },
 		addSlot: { method: 'POST', params:{id:'@id'} },
 		getAll: { method: 'GET', isArray: true, cancellable: true}
 	});
