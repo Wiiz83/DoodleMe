35c3cda20dd6c68a156cce2673ce2702b068fa86
Wiiz83 <uzanlah@gmail.com>
2017-05-13 17:52:21 +0200

    Mise en place des notifs

--- client/app/scripts/directives/header.js
+++ client/app/scripts/directives/header.js
@@ -5,7 +5,13 @@
  	return {
  		templateUrl: 'views/directives/header.html',
  		restrict: 'E',
- 		controller: function($scope, $location) {
+ 		controller: function($scope, $location, $cookieStore, FactoryNotifications) {
+
+ 			var currentUser = $cookieStore.get('id');
+			FactoryNotifications.getCompteur({userID: currentUser}, function(data){
+				$scope.eventsNotified = data;
+			});
+
  			$scope.isActive = function (viewLocation) { 
  				return viewLocation === $location.path();
  			};
---
+++ client/app/scripts/services/notifications.js
@@ -0,0 +1,29 @@
+ 'use strict';
+
+angular.module('clientApp')
+.factory('FactoryNotifications', function ($resource) {
+	return $resource('http://localhost:3000/api/notifications/', {}, {
+
+		markAsRead: { 
+            url:'http://localhost:3000/api/notifications/markasread/',  // POST: {eventID, userID}
+			method: 'POST'
+		},
+
+        getAll : {
+            url:'http://localhost:3000/api/notifications/user/:userID',
+			method: 'GET', 
+			params:{userID:'@id'}, 
+			isArray: true, 
+			cancellable: true
+        },
+        
+        getCompteur : {
+            url:'http://localhost:3000/api/notifications/user/:userID/count',
+			method: 'GET', 
+			params:{userID:'@id'}, 
+			isArray: true, 
+			cancellable: true
+        }
+	});
+});
+
